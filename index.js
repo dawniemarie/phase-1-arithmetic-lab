1e5c78d352e45b5ce9d239bfb1eb7df983a4e908
Dawn Olivier <dmarieolivier@gmail.com>
2022-04-26 22:05:11 -0400

    commit tree 60e676141f9bd4668b8f10c1e9830be88e814cc8

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// multiply //
+let num1 = 31
+let num2 = 2
+let multiply = (num1 * num2)
 
-// Write your code here
+// random //
+let random = Math.floor(Math.random() * 10) + 1;
+
+// mod //
+let num3 = 19
+let num4 = 5
+let mod = (num3 % num4)
+
+// max //
+let max = Math.max (15, 16, 17, 18, 19, 20);
